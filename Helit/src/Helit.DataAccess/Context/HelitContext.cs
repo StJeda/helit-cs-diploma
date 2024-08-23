197e00ecda087051c9558a83991891136e15a0c0
Kagucuti <yyarmolenko17@gmail.com>
2024-08-23 18:53:08 +0300

    added a documentation

--- Helit/src/Helit.DataAccess/Context/HelitContext.cs
+++ Helit/src/Helit.DataAccess/Context/HelitContext.cs
@@ -1,35 +1,17 @@
 ﻿using Helit.Domain.Models;
-using Microsoft.EntityFrameworkCore;
-using MongoDB.EntityFrameworkCore.Extensions;
-using Directory = Helit.Domain.Models.Directory;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
 using File = Helit.Domain.Models.File;
 
 namespace Helit.DataAccess.Context;
 
-public class HelitContext : DbContext
+public class HelitContext
 {
-    public HelitContext(DbContextOptions options) : base(options)
+    private readonly IMongoCollection<Project> _projectsCollection;
+    private readonly IMongoCollection<File> _filesCollection;
+
+    public HelitContext(IOptions<MongoDBSettings> mongoDbSettings)
     {
         
     }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
-
-        modelBuilder.Entity<Project>(e =>
-            e.ToCollection("projects")
-        );
-        modelBuilder.Entity<Directory>(e =>
-            e.ToCollection("directories")
-        );
-        modelBuilder.Entity<File>(e =>
-            e.ToCollection("files")
-        );
-    }
-
-    public DbSet<Project> Projects { get; set; }
-    public DbSet<ProjectConfig> ProjectConfigs { get; set; }
-    public DbSet<Directory> Directories { get; set; }
-    public DbSet<File> Files { get; set; }
 }
